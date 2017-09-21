7da442454d76a90dd2a4310ed23ed3c40e45dd03
Alexandre MOYNIER <32021972+Mizyho@users.noreply.github.com>
2017-09-21 16:06:01 +0200

    Revert "temps de résidence avec bcp de bugs"
    This reverts commit 0be87eea387e2002322fed14a1e442cc3b7615f6.

--- main.py
+++ main.py
@@ -10,33 +10,31 @@ Created on Fri Sep 15 23:44:20 2017
 from time import time
 a=time()
 import module_akmc as mc
-import functions as fn
-n=1000
+from random import randint
 size = 15
 e_aa = 0
 e_ab = 0
 e_bb = 0.21
 proportion_b = 0.5
-Temperature=297 #EN kelvin
+
 systeme = mc.system (size)
 systeme.set_maille([(0,0),(0.5,0.5)])
 systeme.set_link_energy([e_aa,e_ab,e_bb])
 systeme.initiate_map ()
 
-fn.monte_carlo(systeme,proportion_b,"residence-time",n)
-#nombre_b = int(systeme.get_site_number()*proportion_b)
-#print (nombre_b)
-#while nombre_b > 0 :
- #   n = randint(0,systeme.get_site_number()-1) # tirage d'un site au hasard
-  #  if not systeme.get_map()[n].get_identity() : #
-   #     nombre_b -= 1
-    #    systeme.update_map(n,True)
+nombre_b = int(systeme.get_site_number()*proportion_b)
+print (nombre_b)
+while nombre_b > 0 :
+    n = randint(0,systeme.get_site_number()-1) # tirage d'un site au hasard
+    if not systeme.get_map()[n].get_identity() : #
+        nombre_b -= 1
+        systeme.update_map(n,True)
         
 
-#file= open("coordonnee.txt","w")
-#file.writelines(str(systeme.get_site_number())+"\n")
-#file.writelines("\n")
-#for i in systeme.get_map() :
- #   file.writelines("{} {:3.1f} {:3.1f} 0 \n".format(i.get_identity(), *i.get_coordinate() ) )
+file= open("coordonnee.txt","w")
+file.writelines(str(systeme.get_site_number())+"\n")
+file.writelines("\n")
+for i in systeme.get_map() :
+    file.writelines("{} {:3.1f} {:3.1f} 0 \n".format(i.get_identity(), *i.get_coordinate() ) )
 
 print (time()-a)
